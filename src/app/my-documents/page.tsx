339b78127dc60cc34b1fa1a2209d16ed92b26163
adithyatito <adithya_akash@tito.org.in>
2024-06-28 17:00:17 +0530

    upload-functionality-final

--- src/app/my-documents/page.tsx
+++ src/app/my-documents/page.tsx
@@ -5,107 +5,141 @@
 import CommonLayout from "@/components/CommonLayout";
 import * as React from "react";
 import {
-  Box,
-  Breadcrumbs,
-  Button,
-  Container,
-  InputBase,
-  Link,
-  Tab,
-  Typography,
-  alpha,
-  styled,
+	Box,
+	Breadcrumbs,
+	Button,
+	Container,
+	InputBase,
+	Link,
+	Tab,
+	Typography,
+	alpha,
+	styled,
 } from "@mui/material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import HomeIcon from "@mui/icons-material/Home";
 import { Add } from "@mui/icons-material";
-
+import { StorageManager } from "@aws-amplify/ui-react-storage";
+import "@aws-amplify/ui-react/styles.css";
 // import styles from '../../styles/addNewPage.module.css';
 
-
 export default function MyDocuments() {
-  const [value, setValue] = React.useState("1");
+	const [value, setValue] = React.useState("1");
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+	const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+		setValue(newValue);
+	};
 
-  return (
-    <CommonLayout>
-      {/* <Box sx={{ width: "100%", typography: "body1", mt: 2, px: 2, ml: 0 }}> */}
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link
-            underline="hover"
-            sx={{ display: "flex", alignItems: "center" }}
-            color="inherit"
-            href="my-documents/"
-          >
-            <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          </Link>
-          <Link
-            underline="hover"
-            sx={{ display: "flex", alignItems: "center" }}
-            color="inherit"
-          >
-            My Documents
-          </Link>
-        </Breadcrumbs>
-        <Typography variant="h4" component="h2">
-          My Documents
-        </Typography>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
-            <TabList
-              onChange={handleChange}
-              aria-label="Document tabs"
-              centered
-              textColor="primary"
-              indicatorColor="primary"
-              variant="scrollable" // Makes tabs scrollable on small screens
-              scrollButtons="auto"
-            >
-              <Tab label="VERIFIED" value="1" />
-              <Tab label="UNVERIFIED" value="2" />
-              <Tab label="IN PROGRESS" value="3" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">
-        
-          </TabPanel>
-          <TabPanel value="2">
-           
-          </TabPanel>
-          <TabPanel value="3">
-        
-          </TabPanel>
-        </TabContext>
-      {/* </Box> */}
-     
+	return (
+		<CommonLayout>
+			{/* <Box sx={{ width: "100%", typography: "body1", mt: 2, px: 2, ml: 0 }}> */}
+			<Breadcrumbs aria-label='breadcrumb'>
+				<Link
+					underline='hover'
+					sx={{ display: "flex", alignItems: "center" }}
+					color='inherit'
+					href='my-documents/'
+				>
+					<HomeIcon
+						sx={{ mr: 0.5 }}
+						fontSize='inherit'
+					/>
+				</Link>
+				<Link
+					underline='hover'
+					sx={{ display: "flex", alignItems: "center" }}
+					color='inherit'
+				>
+					My Documents
+				</Link>
+			</Breadcrumbs>
+			<Typography
+				variant='h4'
+				component='h2'
+			>
+				My Documents
+			</Typography>
+			<TabContext value={value}>
+				<Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
+					<TabList
+						onChange={handleChange}
+						aria-label='Document tabs'
+						centered
+						textColor='primary'
+						indicatorColor='primary'
+						variant='scrollable' // Makes tabs scrollable on small screens
+						scrollButtons='auto'
+					>
+						<Tab
+							label='VERIFIED'
+							value='1'
+						/>
+						<Tab
+							label='UNVERIFIED'
+							value='2'
+						/>
+						<Tab
+							label='IN PROGRESS'
+							value='3'
+						/>
+					</TabList>
+				</Box>
+				<TabPanel value='1'></TabPanel>
+				<TabPanel value='2'></TabPanel>
+				<TabPanel value='3'></TabPanel>
+			</TabContext>
+			{/* </Box> */}
 
-      
-      <Box sx={{ mt: 20, textAlign: 'center' }}>
-        <Typography variant="h6" component="h1" gutterBottom   color=" #323B4A">
-          There Are No Verified Documents
-        </Typography>
-        
-            <Button
-              variant="contained"
-              size="large"
-              startIcon={<Add />}
-              sx={{
-                // width: '100%',
-                // height: '56px',
-                // fontSize: '1rem',
-                // fontWeight: 'bold',
-                // marginLeft:'10%'
-                textAlign: 'center'
-              }}
-            >
-              ADD NEW
-            </Button>
-        
-      </Box>
-   
-    </CommonLayout>
-  );
+			<Box sx={{ mt: 20, textAlign: "center" }}>
+				<Typography
+					variant='h6'
+					component='h1'
+					gutterBottom
+					color=' #323B4A'
+				>
+					There Are No Verified Documents
+				</Typography>
+
+				<Button
+					variant='contained'
+					size='large'
+					startIcon={<Add />}
+					sx={{
+						// width: '100%',
+						// height: '56px',
+						// fontSize: '1rem',
+						// fontWeight: 'bold',
+						// marginLeft:'10%'
+						textAlign: "center",
+					}}
+					onClick={() => {
+						document.getElementById("file_upload")?.click();
+					}}
+				>
+					ADD NEW
+				</Button>
+			</Box>
+			<Box sx={{ mt: 5, mb: 5, textAlign: "center" }}>
+				<StorageManager
+					acceptedFileTypes={["*"]}
+					path={({ identityId }) => `protected/${identityId}/`}
+					maxFileCount={5}
+					isResumable
+					components={{
+						FilePicker({ onClick }) {
+							return (
+								<Button
+									variant='outlined'
+									id='file_upload'
+									onClick={onClick}
+								>
+									Upload Document
+								</Button>
+							);
+						},
+					}}
+				/>
+			</Box>
+		</CommonLayout>
+	);
 }
--- src/components/DocumentCard.tsx
+++ src/components/DocumentCard.tsx
@@ -1,43 +1,94 @@
 // components/DocumentCard.tsx
-
-import React from 'react';
-import { Card, CardContent, Typography, Button, Box, Link } from '@mui/material';
-import UploadFileIcon from '@mui/icons-material/UploadFile';
-import SearchIcon from '@mui/icons-material/Search';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+"use client";
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import {
+	Card,
+	CardContent,
+	Typography,
+	Button,
+	Box,
+	Link,
+} from "@mui/material";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 type DocumentCardProps = {
-  title: string;
-  description: string;
-  buttonText?: string;
-  linkText?: string;
-  icon: React.ReactNode;
-  buttonLink?: string;
-  linkHref?: string;
+	title: string;
+	description: string;
+	buttonText?: string;
+	linkText?: string;
+	icon: React.ReactNode;
+	buttonLink?: string;
+	linkHref?: string;
 };
 
-const DocumentCard: React.FC<DocumentCardProps> = ({ title, description, buttonText, linkText, icon, buttonLink, linkHref }) => {
-  return (
-    <Card sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 2, borderRadius: '8px' }}>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        {icon}
-        <Box sx={{ marginLeft: 2 }}>
-          <Typography variant="h6" component="div">{title}</Typography>
-          <Typography variant="body2" color="text.secondary">{description}</Typography>
-        </Box>
-      </Box>
-      {buttonText && buttonLink && (
-        <Button variant="contained" color="primary" startIcon={<FileUploadOutlinedIcon />} href={buttonLink}>
-          {buttonText}
-        </Button>
-      )}
-      {linkText && linkHref && (
-        <Link href={linkHref} underline="none" sx={{ display: 'flex', alignItems: 'center', color: 'primary.main' }}>
-          {linkText} <ArrowForwardIosIcon sx={{ fontSize: 16, marginLeft: 1 }} />
-        </Link>
-      )}
-    </Card>
-  );
+const DocumentCard: React.FC<DocumentCardProps> = ({
+	title,
+	description,
+	buttonText,
+	linkText,
+	icon,
+	buttonLink,
+	linkHref,
+}) => {
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const handleNavigation = (path: string) => {
+		router.push(path);
+	};
+	return (
+		<Card
+			sx={{
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				padding: 2,
+				borderRadius: "8px",
+			}}
+		>
+			<Box sx={{ display: "flex", alignItems: "center" }}>
+				{icon}
+				<Box sx={{ marginLeft: 2 }}>
+					<Typography
+						variant='h6'
+						component='div'
+					>
+						{title}
+					</Typography>
+					<Typography
+						variant='body2'
+						color='text.secondary'
+					>
+						{description}
+					</Typography>
+				</Box>
+			</Box>
+			{buttonText && buttonLink && (
+				<Button
+					variant='contained'
+					color='primary'
+					startIcon={<FileUploadOutlinedIcon />}
+					href={buttonLink}
+					onClick={() => handleNavigation("/my-documents")}
+				>
+					{buttonText}
+				</Button>
+			)}
+			{linkText && linkHref && (
+				<Link
+					href={linkHref}
+					underline='none'
+					sx={{ display: "flex", alignItems: "center", color: "primary.main" }}
+				>
+					{linkText}{" "}
+					<ArrowForwardIosIcon sx={{ fontSize: 16, marginLeft: 1 }} />
+				</Link>
+			)}
+		</Card>
+	);
 };
 
 export default DocumentCard;
