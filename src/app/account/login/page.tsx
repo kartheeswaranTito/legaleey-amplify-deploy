d79cbe692570995a75515f7e901ba7d402d09ac6
Bisiranth <bisiranth@tito.org.in>
2024-07-04 23:42:47 +0530

    history page with img

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,47 +1,5 @@
 "use client"
 
-// // App.js
-
-// import React from 'react';
-// import { Amplify } from 'aws-amplify'
-// import { Authenticator } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/styles.css'
-// import awsExports from './aws-exports';
-// Amplify.configure(awsExports);
-
-// export default function App() {
-//   return (
-//     <Authenticator>
-//       {({ signOut, user }) => (
-//         <main>
-//           <h1>Welcome, {user.username}!</h1>
-//           <button onClick={signOut}>Sign out</button>
-//         </main>
-//       )}
-//     </Authenticator>
-  
-//   );
-// }
-
-// import React from 'react';
-// import { Amplify } from 'aws-amplify';
-// import '@aws-amplify/ui-react/styles.css';
-// import { withAuthenticator} from '@aws-amplify/ui-react';
-// import awsconfig from './../../../aws-exports';
-// import Home from '@/app/home/page';
-
-// Amplify.configure(awsconfig);
-
-// function Login({ signOut }) {
-//   return (
-//     <div>
-//         <Home/>
-//       <button onClick={signOut}>Sign out</button>
-//     </div>
-//   );
-// }
-
-// export default withAuthenticator(Login);
 
 import { Amplify } from 'aws-amplify';
 
--- src/app/search-history/page.tsx
+++ src/app/search-history/page.tsx
@@ -1,6 +1,39 @@
+// import CommonLayout from "@/components/CommonLayout";
+// import { Box, Breadcrumbs, Link, Typography } from "@mui/material";
+// import HomeIcon from "@mui/icons-material/Home";
+
+// export default function Trash() {
+//   return (
+//     <CommonLayout>
+//       <Breadcrumbs aria-label="breadcrumb">
+//         <Link
+//           underline="hover"
+//           sx={{ display: "flex", alignItems: "center" }}
+//           color="inherit"
+//           href="search-history/"
+//         >
+//           <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+//         </Link>
+//         <Link
+//           underline="hover"
+//           sx={{ display: "flex", alignItems: "center" }}
+//           color="inherit"
+//         >
+//           Search History
+//         </Link>
+//       </Breadcrumbs>
+//       <Typography variant="h4" component="h2">
+//         Search History
+//       </Typography>
+//     </CommonLayout>
+//   );
+// }
+
 import CommonLayout from "@/components/CommonLayout";
 import { Box, Breadcrumbs, Link, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
+import Image from "next/image"; 
+// import '@fontsource/roboto/600.css';
 
 export default function Trash() {
   return (
@@ -10,7 +43,7 @@ export default function Trash() {
           underline="hover"
           sx={{ display: "flex", alignItems: "center" }}
           color="inherit"
-          href="search-history/"
+          href="/search-history/"
         >
           <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
         </Link>
@@ -26,8 +59,40 @@ export default function Trash() {
         Search History
       </Typography>
 
-      <Box sx={{ mt: 20, textAlign: "center" }}>
-        <Typography variant="h6" component="h1" gutterBottom color=" #323B4A">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "calc(100vh - 200px)",
+          textAlign: "center",
+          position: "relative",
+          mt: 2,
+        }}
+      >
+        <Box sx={{ position: "relative", width: 301.32, height: 165.26 }}>
+          <Image
+            src="/images/history.png" // Correct path starting with a leading slash
+            alt="Search History"
+            layout="fill" // Fill the container
+            objectFit="contain" // Maintain aspect ratio
+          />
+        </Box>
+        <Typography
+          variant="h6"
+          component="h1"
+          gutterBottom
+          sx={{
+            color: "#323B4A",
+            fontFamily: "Roboto",
+            fontSize: "16px",
+            fontWeight: "600",
+            lineHeight: "22px",
+            letterSpacing: "0.46px",
+            mt: 2,
+          }}
+        >
           There is no search History!
         </Typography>
       </Box>
