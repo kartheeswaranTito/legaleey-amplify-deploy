9317d733906b688fcde07cfd8a6d1c6c3ddf45f0
adithyatito <adithya_akash@tito.org.in>
2024-07-25 14:05:27 +0530

    basic ui structure of component

--- src/app/search-results/page.tsx
+++ src/app/search-results/page.tsx
@@ -3,5 +3,5 @@ import * as React from "react";
 import SelectColumn from "../../components/selectColumn";
 
 export default function SearchResults() {
-	<SelectColumn />;
+	return <SelectColumn />;
 }
--- src/components/selectColumn.tsx
+++ src/components/selectColumn.tsx
@@ -1,10 +1,25 @@
 import * as React from "react";
 import { useState } from "react";
-import { Drawer, Button } from "@mui/material";
-import { bool } from "aws-sdk/clients/signer";
+import {
+	Box,
+	Drawer,
+	Button,
+	Typography,
+	IconButton,
+	TextField,
+	Switch,
+} from "@mui/material";
+import { Close, Edit } from "@mui/icons-material";
 
+const drawerWidth = "300px";
 function SelectColumn() {
 	const [selectColumnOpen, setSelectColumnOpen] = useState(false);
+	const [column1Name, setColumn1Name] = useState("Party");
+	const [column2Name, setColumn2Name] = useState("Clause Number");
+	const [column3Name, setColumn3Name] = useState("Jurisdiction");
+	const [column4Name, setColumn4Name] = useState("Governing Law");
+	const [column5Name, setColumn5Name] = useState("Execution Date");
+	const [column6Name, setColumn6Name] = useState("Termination Date");
 
 	function handleSelectColumnChange(nowOpen: boolean) {
 		setSelectColumnOpen(nowOpen);
@@ -18,7 +33,137 @@ function SelectColumn() {
 			<Drawer
 				open={selectColumnOpen}
 				onClose={() => handleSelectColumnChange(false)}
-			></Drawer>
+				anchor='right'
+				sx={{
+					width: drawerWidth,
+					zIndex: -1,
+					flexShrink: 0,
+					[`& .MuiDrawer-paper`]: {
+						width: drawerWidth,
+						boxSizing: "border-box",
+						backgroundColor: "#F7F9FC",
+					},
+				}}
+			>
+				<Box
+					sx={{
+						marginTop: "70px",
+						padding: "15px",
+					}}
+					component='div'
+				>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+
+							marginBottom: "20px",
+						}}
+					>
+						<Typography
+							sx={{
+								fontWeight: "bold",
+								alignSelf: "center",
+								fontSize: "18px",
+							}}
+						>
+							Choose Column to Display
+						</Typography>
+						<IconButton onClick={() => handleSelectColumnChange(false)}>
+							<Close />
+						</IconButton>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+						}}
+					>
+						<Typography>{column1Name}</Typography>
+						<Box>
+							<IconButton>
+								<Edit />
+							</IconButton>
+							<Switch defaultChecked />
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginTop: "20px",
+						}}
+					>
+						<Typography>{column2Name}</Typography>
+						<Box>
+							<IconButton>
+								<Edit />
+							</IconButton>
+							<Switch defaultChecked />
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginTop: "20px",
+						}}
+					>
+						<Typography>{column3Name}</Typography>
+						<Box>
+							<IconButton>
+								<Edit />
+							</IconButton>
+							<Switch defaultChecked />
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginTop: "20px",
+						}}
+					>
+						<Typography>{column4Name}</Typography>
+						<Box>
+							<IconButton>
+								<Edit />
+							</IconButton>
+							<Switch defaultChecked />
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginTop: "20px",
+						}}
+					>
+						<Typography>{column5Name}</Typography>
+						<Box>
+							<IconButton>
+								<Edit />
+							</IconButton>
+							<Switch defaultChecked />
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							marginTop: "20px",
+						}}
+					>
+						<Typography>{column6Name}</Typography>
+						<Box>
+							<IconButton>
+								<Edit />
+							</IconButton>
+							<Switch defaultChecked />
+						</Box>
+					</Box>
+				</Box>
+			</Drawer>
 		</>
 	);
 }
