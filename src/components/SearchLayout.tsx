164dff9ffd9ac11b01b85bb72fd0a52d9d3ee976
Manishasubburajs <manisha@tito.org.in>
2024-07-25 22:40:43 +0530

    search result page layout

---
+++ src/components/SearchLayout.tsx
@@ -0,0 +1,22 @@
+"use client";
+// components/CommonLayout.tsx
+import React, { ReactNode } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import TopNav_Search from './TopNav_Search';
+
+interface SearchLayoutProps {
+  children: ReactNode;
+}
+
+const SearchLayout: React.FC<SearchLayoutProps> = ({ children }) => {
+  return (
+    <Authenticator.Provider>
+    <div>
+      <TopNav_Search />
+      
+    </div>
+    </Authenticator.Provider>
+  );
+};
+
+export default SearchLayout;
