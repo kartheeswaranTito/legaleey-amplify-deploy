9eb0fdbd41efdf11836180326d77bd1b4268b9aa
adithyatito <adithya_akash@tito.org.in>
2024-07-29 10:41:25 +0530

    delete temporary search results page

--- src/app/search-results/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import * as React from "react";
-import SelectColumn from "../../components/select-Column/selectColumn";
-
-export default function SearchResults() {
-	return <SelectColumn />;
-}
