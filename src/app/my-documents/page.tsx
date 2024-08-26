19e5a32dd963b3dedab82ebd4c4b0ac37f4f5e11
Bisiranth <bisiranth@tito.org.in>
2024-08-26 15:49:18 +0530

    Update page.tsx
    margin+

--- src/app/my-documents/page.tsx
+++ src/app/my-documents/page.tsx
@@ -1,45 +1,27 @@
-
 "use client";
 
+import { useState} from "react";
 import CommonLayout from "@/components/CommonLayout";
-import * as React from "react";
 import { Box, Breadcrumbs, Link, Tab, Typography, Button } from "@mui/material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-
 import HomeIcon from "@mui/icons-material/Home";
 import VerifiedTab from "./verified-tab/verified";
 import UnverifiedTab from "./unverified-tab/unverified";
 import InProgressTab from "./In-Progress-tab/inprogress";
+import AddIcon from '@mui/icons-material/Add';
 import { StorageManager } from "@aws-amplify/ui-react-storage";
 import "@aws-amplify/ui-react/styles.css";
 import { edgeServerAppPaths } from "next/dist/build/webpack/plugins/pages-manifest-plugin";
 
 export default function MyDocuments() {
-    const [value, setValue] = React.useState("1");
-    const [showStorageManager, setShowStorageManager] = React.useState(false);
+    const [value, setValue] = useState("1");
+    const [isNewUser, setIsNewUser] = useState(false);
 
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        
+        if (isNewUser && newValue === "3") return;
         setValue(newValue);
     };
-
-    const handleDragEnter = (event:DragEvent) => {
-        event.preventDefault();
-        setShowStorageManager(true);
-    };
-
-    const handleDragOver = (event:DragEvent) => {
-        event.preventDefault();
-    };
-
-    React.useEffect(() => {
-        window.addEventListener("dragenter", handleDragEnter);
-        window.addEventListener("dragover", handleDragOver);
-
-        return () => {
-            window.removeEventListener("dragenter", handleDragEnter);
-            window.removeEventListener("dragover", handleDragOver);
-        };
-    }, []);
 
     return (
         <CommonLayout>
@@ -50,46 +32,150 @@ export default function MyDocuments() {
                     color='inherit'
                     href='my-documents/'
                 >
-                    <HomeIcon sx={{ mr: 0.5 }} fontSize='inherit' />
+                    <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
                 </Link>
-                <Link
-                    underline='hover'
-                    sx={{ display: "flex", alignItems: "center" }}
-                    color='inherit'
-                >
+                <Link underline="hover" sx={{ display: "flex", alignItems: "center" }} color="inherit">
                     My Documents
                 </Link>
             </Breadcrumbs>
-            <Typography variant='h4' component='h2' className='sub-heading'>
+
+            <Typography variant="h4" component="h2" className="sub-heading">
                 My Documents
             </Typography>
+
             <TabContext value={value}>
                 <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
                     <TabList
                         onChange={handleChange}
-                        aria-label='Document tabs'
-                        textColor='primary'
-                        indicatorColor='primary'
-                        variant='scrollable'
-                        scrollButtons='auto'
+                        aria-label="Document tabs"
+                        textColor="primary"
+                        indicatorColor="primary"
+                        variant="scrollable"
+                        scrollButtons="auto"
                     >
-                        <Tab label='VERIFIED' value='1' />
-                        <Tab label='UNVERIFIED' value='2' />
-                        <Tab label='IN PROGRESS' value='3' />
+                        <Tab label="VERIFIED" value="1" />
+                        <Tab label="UNVERIFIED" value="2" />
+                        <Tab
+                            label="IN PROGRESS"
+                            value="3"
+                            disabled={isNewUser} 
+                        />
                     </TabList>
                 </Box>
-                <TabPanel value='1'>
-                    <VerifiedTab />
-                </TabPanel>
-                <TabPanel value='2'>
-                    <UnverifiedTab />
-                </TabPanel>
-                <TabPanel value='3'>
-                    <InProgressTab />
-                </TabPanel>
+
+                {isNewUser && value === "1" ? (
+                    <Box sx={{ textAlign: "center", mt: 5 }}>
+                        <Box
+                            component="img"
+                            src="./images/docs.png"
+                            alt="Documents img"
+                            sx={{
+                                width: "256.88px",
+                                height: "272.83px",
+                                mx: "auto",
+                                mt: "110px",
+                            }}
+                        />
+                        <Typography variant="h5" component="h1" gutterBottom color="#323B4A"
+                          sx={{
+                            color: "#323B4A",
+                            fontFamily: "Roboto",
+                            fontSize: "16px",
+                            fontWeight: "600",
+                            lineHeight: "22px",
+                            letterSpacing: "0.46px",
+                             mb: 2,
+                          }}
+
+                        >
+                            There Are No Verified Documents
+                        </Typography>
+                        
+                         {/* <Button
+                        variant="contained"
+                        size="large"
+                        startIcon={<AddIcon />}
+                        sx={{
+                            width: "141px",
+                            height: "42px",
+                            mt: "16px",
+                            mx: "auto",
+                            padding: "8px 22px",
+                            backgroundColor: "#397EF3",
+                           boxShadow: "none",
+                           
+                        }}
+                        onClick={() => {
+                            document.getElementById("file_upload")?.click();
+                        }}
+                    >
+                        ADD NEW
+                    </Button> */}
+                    </Box>
+                ) : isNewUser && value === "2" ? (
+                    <Box sx={{ textAlign: "center", mt: 5 }}>
+                        <Box
+                            component="img"
+                            src="./images/docs.png"
+                            alt="Documents img"
+                            sx={{
+                                width: "256.88px",
+                                height: "272.83px",
+                                mx: "auto",
+                                mt: "110px",
+                            }}
+                        />
+                        <Typography variant="h5" component="h1" gutterBottom color="#323B4A"
+                          sx={{
+                            color: "#323B4A",
+                            fontFamily: "Roboto",
+                            fontSize: "16px",
+                            fontWeight: "600",
+                            lineHeight: "22px",
+                            letterSpacing: "0.46px",
+                             mb: 2,
+                          }}
+                  
+                        
+                        >
+                            There Are No UnVerified Documents
+                        </Typography>
+                        {/* <Button
+                        variant="contained"
+                        size="large"
+                        startIcon={<AddIcon />}
+                        sx={{
+                            width: "141px",
+                            height: "42px",
+                            mt: "16px",
+                            mx: "auto",
+                            padding: "8px 22px",
+                            backgroundColor: "#397EF3",
+                           boxShadow: "none",
+                        }}
+                        onClick={() => {
+                            document.getElementById("file_upload")?.click();
+                        }}
+                    >
+                        ADD NEW
+                    </Button> */}
+                    </Box>
+                ) : (
+                    <>
+                        <TabPanel value="1">
+                            <VerifiedTab />
+                        </TabPanel>
+                        <TabPanel value="2">
+                            <UnverifiedTab />
+                        </TabPanel>
+                        <TabPanel value="3">
+                            <InProgressTab />
+                        </TabPanel>
+                    </>
+                )}
             </TabContext>
-            <Box sx={{ padding: 5 }}>
-                {showStorageManager && (
+            <Box sx={{ padding: 5, mt:-5,}}>
+                {StorageManager && (
                     <StorageManager
                         acceptedFileTypes={["*"]}
                         path='public/'
@@ -114,3 +200,4 @@ export default function MyDocuments() {
         </CommonLayout>
     );
 }
+
